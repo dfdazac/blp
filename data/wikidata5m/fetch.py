bb5e338f27859adeeab2cba111e2af7801f43f42
Daniel <dfdazac@gmail.com>
2020-02-01 15:43:32 +0100

    Add get_retry()

--- data/wikidata5m/fetch.py
+++ data/wikidata5m/fetch.py
@@ -3,24 +3,23 @@ entities. The list is retrieved from entities.txt
 """
 
 import requests
-from pprint import pprint
 from tqdm import tqdm
+import time
 
 # Maximum number of entities allowed by Wiki APIs
 MAX_ENTITIES = 50
 
-f = open('entities.txt')
-entities = []
+WIKIDATA_BASE_URL = 'https://www.wikidata.org/w/api.php' \
+                    '?action=wbgetentities&props=sitelinks|labels' \
+                    '&languages=en&sitefilter=enwiki&format=json'
 
-WIKIDATA_BASE_REQUEST = 'https://www.wikidata.org/w/api.php' \
-                        '?action=wbgetentities&' \
-                        '&props=sitelinks&sitefilter=enwiki&format=json'
+WIKIPEDIA_BASE_URL = 'https://en.wikipedia.org/w/api.php' \
+                     '?format=json&action=query&prop=extracts&exintro' \
+                     '&explaintext&redirects=1'
 
-WIKIPEDIA_BASE_REQUEST = 'https://en.wikipedia.org/w/api.php' \
-                         '?format=json&action=query&prop=extracts&exintro' \
-                         '&explaintext&redirects=1'
-
+IN_FNAME = 'entities.txt'
 OUT_FNAME = 'descriptions.txt'
+NO_FNAME = 'no_wiki.txt'
 
 
 def get_extracts_from_pages(pages):
@@ -42,14 +41,28 @@ def get_extracts_from_pages(pages):
     return extracts
 
 
+def get_retry(url, params, delay=5):
+    """Call requests.get() with delay to retry, if there is a connection
+    error."""
+    while True:
+        try:
+            return requests.get(url, params)
+        except requests.exceptions.ConnectionError:
+            time.sleep(delay)
+            continue
+
+
+f = open(IN_FNAME)
+entities = []
+
 # Read entities to fetch
 for i, line in enumerate(f):
     entities.append(line.rstrip('\n'))
-    if i == 1000:
-        break
 
 no_wiki_count = 0
+fetched_count = 0
 out_file = open(OUT_FNAME, 'w')
+no_wifi_file = open(NO_FNAME, 'w')
 
 print('Retrieving Wikipedia page titles...', flush=True)
 for i in tqdm(range(0, len(entities), MAX_ENTITIES)):
@@ -58,24 +71,25 @@ for i in tqdm(range(0, len(entities), MAX_ENTITIES)):
     ids_param = '|'.join(to_fetch)
 
     # Request Wikipedia page titles from Wikidata
-    r = requests.get(WIKIDATA_BASE_REQUEST, params={'ids': ids_param})
+    r = get_retry(WIKIDATA_BASE_URL, params={'ids': ids_param})
     link_data = r.json()['entities']
     ent_pages = []
 
     for e in to_fetch:
-        # Proceed only if enwiki page exists
+        # Check if enwiki page exists
         if 'enwiki' in link_data[e]['sitelinks']:
             title = link_data[e]['sitelinks']['enwiki']['title']
             ent_pages.append((e, title))
         else:
             no_wiki_count += 1
+            no_wifi_file.write(f'{e}\n')
 
     titles = [title for (e, title) in ent_pages]
     titles_param = '|'.join(titles)
-    req_url = WIKIPEDIA_BASE_REQUEST
+    req_url = WIKIPEDIA_BASE_URL
 
     # Request first Wikipedia section
-    r = requests.get(WIKIPEDIA_BASE_REQUEST, params={'titles': titles_param})
+    r = get_retry(WIKIPEDIA_BASE_URL, params={'titles': titles_param})
 
     text_data = r.json()
     extracts = get_extracts_from_pages(text_data['query']['pages'])
@@ -93,12 +107,15 @@ for i in tqdm(range(0, len(entities), MAX_ENTITIES)):
 
     # Save to file
     for (entity, title) in ent_pages:
-        # Was there a redirect?
+        # If there was a redirect, change title accordingly
         if title in redir_titles:
             title = redir_titles[title]
 
-        out_file.write(f'{entity} {title} #### {extracts[title]}\n')
+        out_file.write(f'{entity} {title}: {extracts[title]}\n')
+        fetched_count += 1
 
 
-print(f'{no_wiki_count:d} entities with no Wikipedia page.')
-print(f'Saved entities to {OUT_FNAME}')
+print(f'Retrieved {fetched_count:d} pages.'
+      f'There were {no_wiki_count:d} entities with no Wikipedia page.')
+print(f'Saved entities and pages in {OUT_FNAME}')
+print(f'Saved entities with no pages in {NO_FNAME}')
