04651186981eb906c82aecf24375262bcaba3a13
Daniel <dfdazac@gmail.com>
2020-02-05 22:01:14 +0100

    Add TransE model

--- graph.py
+++ graph.py
@@ -7,21 +7,26 @@ class TransE(nn.Module):
     def __init__(self, num_entities, num_relations, dim, margin=1):
         super(TransE, self).__init__()
 
+        # TODO: Initialize
         self.ent_emb = nn.Embedding(num_entities, dim)
         self.rel_emb = nn.Embedding(num_relations, dim)
         self.margin = margin
 
+    def energy(self, triples):
+        head, rel, tail = torch.chunk(triples, chunks=3, dim=1)
+
+        h = F.normalize(self.ent_emb(head), dim=-1)
+        r = F.normalize(self.rel_emb(rel), dim=-1)
+        t = F.normalize(self.ent_emb(tail), dim=-1)
+
+        energy = torch.norm(h + r - t, dim=-1)
+        return energy
+
     def forward(self, data):
-        head, pos_tail, neg_tail, rel = torch.chunk(data, chunks=4, dim=1)
+        pos_triples, neg_triples = data
 
-        h_emb = F.normalize(self.ent_emb(head), dim=-1)
-        t_pos_emb = F.normalize(self.ent_emb(pos_tail), dim=-1)
-        t_neg_emb = F.normalize(self.ent_emb(neg_tail), dim=-1)
-        rel_emb = F.normalize(self.rel_emb(rel), dim=-1)
-
-        base_energy = h_emb + rel_emb
-        pos_energy = torch.norm(base_energy - t_pos_emb, dim=-1)
-        neg_energy = torch.norm(base_energy - t_neg_emb, dim=-1)
+        pos_energy = self.energy(pos_triples)
+        neg_energy = self.energy(neg_triples)
 
         loss = self.margin + pos_energy - neg_energy
         loss[loss < 0] = 0
