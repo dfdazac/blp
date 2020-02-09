af6c510c2371ebccb4d76606f99909f4e993f30b
Daniel <dfdazac@gmail.com>
2020-02-09 16:36:39 +0100

    Deal with unseen entities in datasets

--- data.py
+++ data.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 import torch
 from torch.utils.data import Dataset
 
+UNK = '[UNK]'
+
 
 class GraphDataset(Dataset):
     """A Dataset storing the triples of a Knowledge Graph.
@@ -16,35 +18,42 @@ class GraphDataset(Dataset):
     """
     def __init__(self, triples_file):
         base_path = osp.dirname(triples_file)
+        filename = osp.basename(triples_file)
         out_path = osp.join(base_path, 'processed')
-        data_path = osp.join(out_path, 'data.pt')
+        data_path = osp.join(out_path, f'{filename}.pt')
         maps_path = osp.join(out_path, 'maps.pt')
 
         if not osp.exists(out_path):
             os.mkdir(out_path)
 
-            # Create maps from entity and relation strings to unique IDs
+        # Create or load maps from entity and relation strings to unique IDs
+        if not osp.exists(maps_path):
             ent_ids = defaultdict(lambda: len(ent_ids))
             rel_ids = defaultdict(lambda: len(rel_ids))
-            triples = []
+            unk_id = ent_ids[UNK]
+        else:
+            maps = torch.load(maps_path)
+            ent_ids, rel_ids = maps['ent_ids'], maps['rel_ids']
+            ent_ids = defaultdict(lambda: ent_ids[UNK], ent_ids)
 
+        if not osp.exists(data_path):
+            # Read triples and store as ints in tensor
             file = open(triples_file)
+            triples = []
             for i, line in enumerate(file):
                 head, rel, tail = line.split()
                 triples.append([ent_ids[head], rel_ids[rel], ent_ids[tail]])
 
-            # Convert to dict for serialization
-            ent_ids = dict(ent_ids)
-            rel_ids = dict(rel_ids)
-
-            # Save separately IDs tensor, and maps
             self.triples = torch.tensor(triples, dtype=torch.long)
             torch.save(self.triples, data_path)
-            torch.save({'ent_ids': ent_ids, 'rel_ids': rel_ids}, maps_path)
+
+            if not osp.exists(maps_path):
+                # Convert maps to dict for serialization
+                ent_ids = dict(ent_ids)
+                rel_ids = dict(rel_ids)
+                torch.save({'ent_ids': ent_ids, 'rel_ids': rel_ids}, maps_path)
         else:
             self.triples = torch.load(data_path)
-            maps = torch.load(maps_path)
-            ent_ids, rel_ids = maps['ent_ids'], maps['rel_ids']
 
         self.num_ents = len(ent_ids)
         self.num_rels = len(rel_ids)
@@ -74,17 +83,3 @@ class GraphDataset(Dataset):
         neg_triples[triple_idx, corrupt_idx] = corrupt_ent
 
         return pos_triples, neg_triples
-
-
-def make_data_iterator(data_loader):
-    """Create a continuous iterator for a DataLoader.
-
-    Args:
-        data_loader: DataLoader to iterate
-    """
-    iterator = iter(data_loader)
-    while True:
-        try:
-            yield next(iterator)
-        except StopIteration:
-            iterator = iter(data_loader)
