c7f8d2c4daccbc97ecb9daaf08cef8ffec087ee5
Daniel <dfdazac@gmail.com>
2020-02-12 22:00:52 +0100

    Use default entity and relation lists in Datasets

--- data.py
+++ data.py
@@ -1,12 +1,28 @@
 import os
 import os.path as osp
 import numpy as np
-from collections import defaultdict
 import torch
 from torch.utils.data import Dataset
+import transformers
 
 UNK = '[UNK]'
 DELIM = '####'
+
+
+def file_to_ids(file_path):
+    """Read one line per file and assign it an ID.
+
+    Args:
+        file_path: str, path of file to read
+
+    Returns: dict, mapping str to ID (int)
+    """
+    str2id = dict()
+    with open(file_path) as file:
+        for i, line in enumerate(file):
+            str2id[line.strip()] = i
+
+    return str2id
 
 
 class GraphDataset(Dataset):
@@ -16,8 +32,12 @@ class GraphDataset(Dataset):
         triples_file: str, path to the file containing triples. This is a
             text file where each line contains a triple of the form
             'subject predicate object'
+        ents_file: str, path to file with a list of unique entities, possibly
+            shared between Datasets.
+        rels_file: str, path to file with a list of unique relations, possibly
+            shared between Datasets.
     """
-    def __init__(self, triples_file):
+    def __init__(self, triples_file, ents_file=None, rels_file=None):
         base_path = osp.dirname(triples_file)
         filename = osp.basename(triples_file)
         out_path = osp.join(base_path, 'processed')
@@ -29,13 +49,14 @@ class GraphDataset(Dataset):
 
         # Create or load maps from entity and relation strings to unique IDs
         if not osp.exists(maps_path):
-            ent_ids = defaultdict(lambda: len(ent_ids))
-            rel_ids = defaultdict(lambda: len(rel_ids))
-            unk_id = ent_ids[UNK]
+            if not ents_file or not rels_file:
+                raise ValueError('Maps file not found.')
+
+            ent_ids = file_to_ids(ents_file)
+            rel_ids = file_to_ids(rels_file)
         else:
             maps = torch.load(maps_path)
             ent_ids, rel_ids = maps['ent_ids'], maps['rel_ids']
-            ent_ids = defaultdict(lambda: ent_ids[UNK], ent_ids)
 
         if not osp.exists(data_path):
             # Read triples and store as ints in tensor
@@ -48,10 +69,8 @@ class GraphDataset(Dataset):
             self.triples = torch.tensor(triples, dtype=torch.long)
             torch.save(self.triples, data_path)
 
+            # Save maps for reuse
             if not osp.exists(maps_path):
-                # Convert maps to dict for serialization
-                ent_ids = dict(ent_ids)
-                rel_ids = dict(rel_ids)
                 torch.save({'ent_ids': ent_ids, 'rel_ids': rel_ids}, maps_path)
         else:
             self.triples = torch.load(data_path)
@@ -89,36 +108,66 @@ class GraphDataset(Dataset):
 
 class TextGraphDataset(GraphDataset):
     """A dataset storing a graph, and textual descriptions of its entities.
+    Note: if text_data is not provided, both text_file and tokenizer must be
+    given.
 
     Args:
         triples_file: str, path to the file containing triples. This is a
             text file where each line contains a triple of the form
             'subject predicate object'
+        ents_file: str, path to file with a list of unique entities, possibly
+            shared between Datasets.
+        rels_file: str, path to file with a list of unique relations, possibly
+            shared between Datasets.
         text_file: str, path to the file containing entity descriptions.
             Assumes one description per line for each entity, starting with
-            the entity ID, followed by the description.
+            the entity ID, followed by its description.
+        tokenizer: transformers.PreTrainedTokenizer
+        text_data: torch.Tensor, containing text IDs obtained from text_file
+            after tokenization. Use to share text data between Datasets.
     """
-    def __init__(self, triples_file, text_file):
-        super(TextGraphDataset, self).__init__(triples_file)
+    def __init__(self, triples_file, ents_file=None, rels_file=None,
+                 text_data=None, text_file=None,
+                 tokenizer: transformers.PreTrainedTokenizer=None):
+        super(TextGraphDataset, self).__init__(triples_file,
+                                               ents_file,
+                                               rels_file)
+        if text_data is None:
+            if text_file is None or tokenizer is None:
+                raise ValueError('If text_data is not provided, both text_file'
+                                 ' and tokenizer must be given.')
+            else:
+                # Check if a serialized version exists, otherwise create
+                maps = torch.load(self.maps_path)
+                ent_ids = maps['ent_ids']
 
-        maps = torch.load(self.maps_path)
-        ent_ids, rel_ids = maps['ent_ids'], maps['rel_ids']
-        ent_ids = defaultdict(lambda: ent_ids[UNK], ent_ids)
+                # Read descriptions, and build a map from entity ID to text
+                ent2text = dict()
+                text = open(text_file)
+                for line in text:
+                    name_start = line.find(' ')
+                    name_end = line.find(DELIM)
+                    entity = line[:name_start].strip()
+                    # TODO: Tokenize and encode
+                    # description = tokenizer.encode()
+                    ent2text[ent_ids[entity]] = line[name_start:name_end].strip()
 
-        # Read descriptions, and build a map from entity ID to text
-        ent2text = dict()
+                # TODO: store indices in tensor
+                self.text_data = torch.tensor([1, 2, 3])
 
-        text = open(text_file)
-
-        for line in text:
-            name_start = line.find(' ')
-            name_end = line.find(DELIM)
-            entity = line[:name_start].strip()
-            ent2text[ent_ids[entity]] = line[name_start:name_end].strip()
-
-        print('Done reading')
+                # Save tensor for serialization
+        else:
+            self.text_data = text_data
 
 
 if __name__ == '__main__':
-    g = TextGraphDataset('data/wikifb15k-237/train.txt',
-                         'data/wikifb15k-237/descriptions.txt')
+    gtr = TextGraphDataset('data/wikifb15k-237/train.txt',
+                           ents_file='data/wikifb15k-237/entities.txt',
+                           rels_file='data/wikifb15k-237/relations.txt',
+                           text_file='data/wikifb15k-237/descriptions.txt')
+    gva = TextGraphDataset('data/wikifb15k-237/valid.txt',
+                           text_data=gtr.text_data)
+    gte = TextGraphDataset('data/wikifb15k-237/test.txt',
+                           text_data=gtr.text_data)
+
+    print('Done')
