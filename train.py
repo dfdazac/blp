d11e138a30eb56cba3cdefc198c0f82bc1fbc9a0
Daniel <dfdazac@gmail.com>
2020-02-25 12:46:24 +0100

    Train alignment model

--- train.py
+++ train.py
@@ -6,7 +6,8 @@ from logging import Logger
 from transformers import AlbertTokenizer, get_linear_schedule_with_warmup
 
 from data import GraphDataset, TextGraphDataset
-from graph import TransE, BERTransE
+from graph import TransE, BERTransE, RelTransE
+from text import SummaryModel, EntityAligner
 import utils
 
 ex = utils.create_experiment()
@@ -24,8 +25,8 @@ def config():
 
 @ex.capture
 @torch.no_grad()
-def evaluate(model, loader, epoch, _run: Run, _log: Logger,
-             prefix='', max_num_batches=None):
+def eval_link_prediction(model, loader, epoch, _run: Run, _log: Logger,
+                         prefix='', max_num_batches=None):
     hits = 0.0
     mrr = 0.0
 
@@ -80,28 +81,29 @@ def evaluate(model, loader, epoch, _run: Run, _log: Logger,
     _run.log_scalar(f'{prefix}_valid_hits@10', hits, epoch)
 
 
-@ex.automain
-def train(dim, lr, margin, p_norm, max_epochs, _run: Run, _log: Logger):
+@ex.command
+def link_prediction(dim, lr, margin, p_norm, max_epochs,
+                    _run: Run, _log: Logger):
     tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
-    train_data = TextGraphDataset(triples_file='data/wikifb15k-237/train.txt',
-                                  ents_file='data/wikifb15k-237/entities.txt',
-                                  rels_file='data/wikifb15k-237/relations.txt',
-                                  text_file='data/wikifb15k-237/descriptions.txt',
-                                  tokenizer=tokenizer)
-    train_loader = DataLoader(train_data, batch_size=32, shuffle=True,
-                              collate_fn=train_data.negative_sampling,
+    dataset = TextGraphDataset(triples_file='data/wikifb15k-237/train.txt',
+                               ents_file='data/wikifb15k-237/entities.txt',
+                               rels_file='data/wikifb15k-237/relations.txt',
+                               text_file='data/wikifb15k-237/descriptions.txt',
+                               tokenizer=tokenizer)
+    train_loader = DataLoader(dataset, batch_size=32, shuffle=True,
+                              collate_fn=dataset.negative_sampling,
                               num_workers=4)
-    train_eval_loader = DataLoader(train_data, batch_size=128)
+    train_eval_loader = DataLoader(dataset, batch_size=128)
 
     valid_data = TextGraphDataset('data/wikifb15k-237/valid.txt',
-                                  text_data=train_data.text_data)
+                                  text_data=dataset.text_data)
     valid_loader = DataLoader(valid_data, batch_size=128)
 
     test_data = TextGraphDataset('data/wikifb15k-237/test.txt',
-                                 text_data=train_data.text_data)
+                                 text_data=dataset.text_data)
     test_loader = DataLoader(test_data, batch_size=128)
 
-    model = BERTransE(train_data.num_ents, train_data.num_rels, dim=dim,
+    model = BERTransE(dataset.num_ents, dataset.num_rels, dim=dim,
                       margin=margin, p_norm=p_norm).to(device)
 
     optimizer = Adam([{'params': model.bert.albert.parameters(), 'lr': lr},
@@ -135,11 +137,96 @@ def train(dim, lr, margin, p_norm, max_epochs, _run: Run, _log: Logger):
         _run.log_scalar('train_loss', train_loss/len(train_loader), epoch)
 
         _log.info('Evaluating on sample of training set...')
-        evaluate(model, train_eval_loader, epoch, prefix='train',
-                 max_num_batches=len(valid_loader))
+        eval_link_prediction(model, train_eval_loader, epoch, prefix='train',
+                             max_num_batches=len(valid_loader))
 
         _log.info('Evaluating on validation set...')
-        evaluate(model, valid_loader, epoch, prefix='valid')
+        eval_link_prediction(model, valid_loader, epoch, prefix='valid')
 
     _log.info('Evaluating on test set...')
-    evaluate(model, test_loader, max_epochs, prefix='test')
+    eval_link_prediction(model, test_loader, max_epochs, prefix='test')
+
+
+def get_entity_summaries(dataset):
+    """Load pretrained entity summaries"""
+    summarizer = SummaryModel()
+    emb_dim = summarizer.encoder.config.hidden_size
+    load_num = 256
+
+    all_ents = torch.arange(dataset.num_ents)
+    ent_summaries = torch.empty((dataset.num_ents, emb_dim))
+
+    # FIXME
+    # for i in range(0, dataset.num_ents, load_num):
+    #     # Get a batch of entity IDs
+    #     batch_ents = all_ents[i:i + load_num]
+    #     # Get their corresponding descriptions
+    #     tokens, masks = dataset.get_entity_descriptions(batch_ents)
+    #     # Encode with BERT and store result
+    #     batch_emb = summarizer(tokens.to(device), masks.to(device))
+    #     ent_summaries[i:i + load_num] = batch_emb
+
+    return ent_summaries
+
+
+@ex.automain
+def train_linker(lr, margin, p_norm, _run: Run, _log: Logger):
+    tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+    dataset = TextGraphDataset(triples_file='data/wikifb15k-237/train.txt',
+                               ents_file='data/wikifb15k-237/entities.txt',
+                               rels_file='data/wikifb15k-237/relations.txt',
+                               text_file='data/wikifb15k-237/descriptions.txt',
+                               tokenizer=tokenizer)
+
+    loader = DataLoader(dataset, batch_size=256, shuffle=True,
+                        collate_fn=dataset.graph_negative_sampling,
+                        num_workers=4)
+
+    summaries = get_entity_summaries(dataset).to(device)
+
+    aligner = EntityAligner().to(device)
+    graph_model = RelTransE(dataset.num_rels, aligner.dim, margin, p_norm)
+    optimizer = Adam([{'params': aligner.parameters()},
+                      {'params': graph_model.parameters()}], lr=1e-5)
+
+    for i in range(dataset.num_ents):
+        # Get entity description
+        ent_id = torch.tensor([i], dtype=torch.long)
+        tokens, mask = dataset.get_entity_descriptions(ent_id)
+        tokens = tokens.to(device)
+        mask = mask.to(device)
+
+        # Use alignment model to obtain entity embeddings
+        ent_embs = aligner(tokens, mask, summaries)
+
+        # Run 1 epoch of TransE
+        for j, data in enumerate(loader):
+            # TODO: check gradient scaling
+            loss = graph_model(*[tensor.to(device) for tensor in data],
+                               ent_embs)
+
+            loss.backward(retain_graph=True)
+            if j % 100 == 0:
+                print(f'{j}/{len(loader)}  {loss.item()}')
+
+        min_grad_all = float('inf')
+        max_grad_all = -float('inf')
+
+        for group in optimizer.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+
+                min_grad = grad.min().item()
+                max_grad = grad.max().item()
+
+                if min_grad <= min_grad_all:
+                    min_grad_all = min_grad
+                if max_grad >= max_grad_all:
+                    max_grad_all = max_grad
+
+        print(f'min_grad: {min_grad_all:.6f}  max_grad: {max_grad_all:.6f}')
+
+        optimizer.step()
+        optimizer.zero_grad()
