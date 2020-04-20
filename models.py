Fix regularization term and neg_scores calculation

models.py
@@ -50,22 +50,26 @@ class LinkPrediction(nn.Module):
 
     def compute_loss(self, ent_embs, rels, neg_idx):
         batch_size = ent_embs.shape[0]
+
         # Scores for positive samples
         rels = self.rel_emb(rels)
         heads, tails = torch.chunk(ent_embs, chunks=2, dim=1)
         pos_scores = self.score_fn(heads, tails, rels)
 
+        if self.regularizer > 0:
+            reg_loss = self.regularizer * l2_regularization(heads, tails, rels)
+        else:
+            reg_loss = 0
+
         # Scores for negative samples
         neg_embs = ent_embs.view(batch_size * 2, -1)[neg_idx]
         heads, tails = torch.chunk(neg_embs, chunks=2, dim=0)
-        neg_scores = self.score_fn(heads.squeeze(), tails.squeeze(), rels)
+        neg_scores = self.score_fn(heads.view(batch_size, -1, self.dim),
+                                   tails.view(batch_size, -1, self.dim),
+                                   rels)
 
-        loss = self.loss_fn(pos_scores, neg_scores)
-
-        if self.regularizer > 0:
-            loss += self.regularizer * l2_regularization(heads, tails, rels)
-
-        return loss
+        model_loss = self.loss_fn(pos_scores, neg_scores)
+        return model_loss + reg_loss
 
 
 class InductiveLinkPrediction(LinkPrediction):